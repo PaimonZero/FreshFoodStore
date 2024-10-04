0ac157b692d439c6a4e7f043b1c323265d773edc
ZeroPaimon <pnam2212004@gmail.com>
2024-10-04 09:56:24 +0700

    dele

--- FreshFoodStore2709/src/java/util/DatabaseInfo.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public interface DatabaseInfo {
-    public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String DBURL="jdbc:sqlserver://TUF-F15-HXNAM\\SQLEXPRESS;databaseName=BanHangOnlineNew;encrypt=false;trustServerCertificate=false;loginTimeout=30"; 
-    public static String USERDB="sa";
-    public static String PASSDB="123";
-   
-}
