20075d5a7f2f96f67b148b39fd3e7de20c9f43ee
PaimonZero <148264786+PaimonZero@users.noreply.github.com>
2024-10-16 00:13:44 +0700

    Update DatabaseInfo.java to online DB

--- 1_Project/FreshFoodStore/src/java/dal/DatabaseInfo.java
+++ 1_Project/FreshFoodStore/src/java/dal/DatabaseInfo.java
@@ -9,9 +9,10 @@ public interface DatabaseInfo {
 
 //Nam
     public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String DBURL="jdbc:sqlserver://TUF-F15-HXNAM\\SQLEXPRESS;databaseName=FreshFoodStore;encrypt=false;trustServerCertificate=false;loginTimeout=30"; 
-    public static String USERDB="sa";
-    public static String PASSDB="123";
+//    public static String DBURL="jdbc:sqlserver://TUF-F15-HXNAM\\SQLEXPRESS;databaseName=FreshFoodStore;encrypt=false;trustServerCertificate=false;loginTimeout=30"; 
+    public static String DBURL="jdbc:sqlserver://freshfoodstore.database.windows.net:1433;database=FreshFoodStore;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"; 
+    public static String USERDB="freshfoodstore";
+    public static String PASSDB="Free2play2004";
 
 //Trung
 //    public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
